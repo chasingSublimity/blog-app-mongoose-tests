2401eedd8e145f8aaa431b2c68c4acfb6acf2a3d
chasingSublimity <blakemsager@gmail.com>
2017-01-18 20:15:23 -0600

    add mLab mongo address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-posts';
+                      'mongodb://chasingSublimity:lavalamp1@ds035740.mlab.com:35740/blog-posts';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-blog-posts');
